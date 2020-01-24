3520ccccdad5aff2a14f5c9025f86b0ca230aa00
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2020-01-24 15:48:29 -0300

    created ficture update user

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -47,6 +47,22 @@ namespace startapidotnet.Controllers
             return Ok(userModel);
         }
 
+        [HttpPut]
+        public IActionResult update([FromBody] UserModel userModel)
+        {
+            UserModel user = this.database.users.First(u => u.id == userModel.id);
+            user.nome = userModel.nome;
+            user.email = userModel.email;
+            user.login = userModel.login;
+            user.password = userModel.password;
+        
+            database.users.Update(user);
+            database.SaveChanges();
+
+            Response.StatusCode = 200;
+            return Ok(userModel);
+        }
+
         [HttpDelete("{id}")]
         public IActionResult delete(int id)
         {
